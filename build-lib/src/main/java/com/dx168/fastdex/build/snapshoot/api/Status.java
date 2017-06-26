689276460857999ab83ab3a9bc15f4fcbae7c745
typ0520 <yapeng.tong@baidao.comc>
2017-06-26 09:19:27 +0800

    remove old module

--- build-lib/src/main/java/com/dx168/fastdex/build/snapshoot/api/Status.java
+++
@@ -1,8 +0,0 @@
-package com.dx168.fastdex.build.snapshoot.api;
-
-/**
- * Created by tong on 17/3/29.
- */
-public enum Status {
-    NOCHANGED, ADDED, DELETEED, MODIFIED
-}
--- build-lib/src/test/java/snapshoot/ResultSetTest.java
+++
@@ -1,7 +0,0 @@
-package snapshoot;
-
-/**
- * Created by tong on 17/3/31.
- */
-public class ResultSetTest {
-}
